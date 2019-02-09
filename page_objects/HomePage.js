0d63268d78a0544c6a567cf7cce6490d89c1be1c
Mateusz Bloch <mateuszbloch90@gmail.com>
2019-02-09 15:55:43 +0100

    Added Search Suggestions Test

--- page_objects/HomePage.js
+++ page_objects/HomePage.js
@@ -1,7 +1,7 @@
 module.exports = class HomePage {
   constructor() {
     this.path = 'http://automationpractice.com/index.php';
-    this.searchQuery = element(by.css('.search_query'));
+    this.inputSearchBox = element(by.css('.search_query'));
     this.searchButton = element(by.name('submit_search'));
     this.signInButtonHeader = element(by.css('.header_user_info'));
     this.emailInput = element(by.id('email'));
--- page_objects/SearchResults.js
+++ page_objects/SearchResults.js
@@ -3,5 +3,11 @@ module.exports = class SearchResults {
     this.searchResultsElement = element(by.css('.heading-counter'));
     this.productGrid = element(by.css('.product_list'));
     this.firstProductName = element(by.css('.product_list')).all(by.css('.product-name')).first();
+    this.searchSuggestion = element(by.css('.ac_results'));
+    this.firstSearchSuggestion = element.all(by.css('.ac_even')).first();
+    this.productHeading = element(by.css('h1'));
+    this.productMainPicture = element(by.id('bigpic'));
+    this.searchQuery = 'Dress';
+    this.noResultsQuery = 'zzzzz###';
   }
 };
